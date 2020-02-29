614bbe62051255cbc7a3107c047ae093a0f887ec
Stela Capsa <stela.capsa@gmail.com>
2020-02-29 13:09:15 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change
-   change_column :students, :datetime, :datatype
+   change_column :students, :birthdate, :datetime
   end 
   
 end 
