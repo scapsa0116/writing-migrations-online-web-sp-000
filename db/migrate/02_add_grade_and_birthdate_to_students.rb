7c86e559f29cd8faa36e98a0fffd3931c23908f9
Stela Capsa <stela.capsa@gmail.com>
2020-02-29 13:07:32 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
-  def change 
-   add_column :grade, :birthdate
-
+ def change
+		add_column :students, :grade, :integer
+		add_column :students, :birthdate, :string
 end 
 end 
